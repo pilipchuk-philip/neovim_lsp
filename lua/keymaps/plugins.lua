d915dc58f74945a196894f4363b908d9b2ae0917
pilipchuk-philip <pilipchuk.philip@gmail.com>
2022-11-05 22:44:15 +0100

    Added Gist and Git

--- lua/keymaps/plugins.lua
+++ lua/keymaps/plugins.lua
@@ -23,22 +23,23 @@ cmd [[ vmap <C-/>   <Plug>kommentary_visual_default ]]
 cmd[[ nnoremap <silent>gD :lua vim.lsp.buf.declaration()<CR> ]]
 cmd[[ nnoremap <silent>gd :lua vim.lsp.buf.definition()<CR> ]]
 -- cmd[[ nnoremap <silent>K :lua vim.lsp.buf.hover()<CR> ]]
------------------------------------------------------------------------
---- LSP Saga
------------------------------------------------------------------------ 
 -- Hover Doc
 keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", { silent = true })
 -- Rename in current file
 keymap("n", "gr", "<cmd>Lspsaga rename<CR>", { silent = true })
- 
+
 -----------------------------------------------------------------------
 -- TODO 
 ----------------------------------------------------------------------- 
 cmd [[ nmap <C-t> :TodoQuickFix<CR> ]]
- -----------------------------------------------------------------------
+-----------------------------------------------------------------------
 -- TAGS 
 -----------------------------------------------------------------------  
 cmd[[ nnoremap <silent>s :SymbolsOutline<CR> ]]
+-----------------------------------------------------------------------
+-- GIT 
+-----------------------------------------------------------------------   
+cmd[[ nnoremap <silent>g :Neogit<CR> ]]
 
 -----------------------------------------------------------------------
 -- Telescope 
@@ -49,3 +50,32 @@ cmd [[ nnoremap <C-e> :Telescope buffers<CR> ]]
 cmd [[ nnoremap <C-g> :Telescope git_status<CR> ]]
 cmd [[ nnoremap <C-l> :Telescope git_status<CR> ]]
 
+-----------------------------------------------------------------------
+--- LSP Saga
+----------------------------------------------------------------------- 
+keymap("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", { silent = true })
+
+-- Code action
+-- keymap({"n","v"}, "<leader>ca", "<cmd>Lspsaga code_action<CR>", { silent = true })
+keymap({"n"}, "<CR>", "<cmd>Lspsaga code_action<CR>", { silent = true })
+
+-- Show line diagnostics
+keymap("n", "<leader>cd", "<cmd>Lspsaga show_line_diagnostics<CR>", { silent = true })
+
+-- Show cursor diagnostic
+keymap("n", "<leader>cd", "<cmd>Lspsaga show_cursor_diagnostics<CR>", { silent = true })
+
+-- Diagnsotic jump can use `<c-o>` to jump back
+keymap("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { silent = true })
+keymap("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", { silent = true })
+
+-- Only jump to error
+keymap("n", "[E", function()
+  require("lspsaga.diagnostic").goto_prev({ severity = vim.diagnostic.severity.ERROR })
+end, { silent = true })
+keymap("n", "]E", function()
+  require("lspsaga.diagnostic").goto_next({ severity = vim.diagnostic.severity.ERROR })
+end, { silent = true })
+
+-- Outline
+keymap("n","<leader>o", "<cmd>LSoutlineToggle<CR>",{ silent = true })
